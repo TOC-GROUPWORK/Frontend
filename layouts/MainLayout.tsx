ddeeeb773cb75a65c1255e8e9a887a570510b882
Sirawit <popeye-zz@hotmail.com>
2022-04-17 02:49:33 +0700

    add footer, edit some navbar

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
 import NavBar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const MainLayout = ({ children }: { children: React.ReactElement}) => (
     <div>
         <NavBar/>
         {children}
-        {/* Footer */}
+        <Footer/>
     </div>
 );
 
